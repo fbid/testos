3f01dab99e9123c2b23f67622cca040fc03301f8
fbid <federico.bidoggia@gmail.com>
2015-09-12 00:14:28 +0200

    update with json dir content resp

--- app.js
+++ app.js
@@ -30,55 +30,66 @@ function processa(req, res) {
     var uri = url.parse(req.url).pathname;
     var filename = path.join(process.cwd(), decodeURI(uri));
     stato_file(filename,
-        function (err, stats) {
-            console.log("I'm processing " + filename + '\n');
-            if (err) {
-                res.writeHead(404, {
-                    'Content-Type': 'text/plain'
-                });
-                res.write('404 Resource Not Found\n');
-                res.end();
-                return;
+            function (err, stats) {
+                console.log("I'm processing " + filename + '\n');
+                if (err) {
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    });
+                    res.write('404 Resource Not Found\n');
+                    res.end();
+                    return;
+                }
+                // se la richiesta corrisponde a un file
+                if (stats.isFile()) {
+                    var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
+
+                    res.writeHead(200, {
+                        'Content-Type': mimeType
+                    });
+                    // crea uno stream dal file e lo va a scrivere all 'interno di res
+                    var fileStream = fs.createReadStream(filename);
+                    fileStream.pipe(res);
+                } else if (stats.isDirectory()) {
+                    // se la richiesta corrisponde a un file directory
+                    res.writeHead(200, {
+                        'Content-Type': 'text/plain'
+                    });
+                    fs.readdir(filename,function (err, files) {
+                            var rescon = "";
+                            if (err) {
+                                rescon = err;
+                            } else {
+                                rescon = {
+                                    error: err,
+                                    data: {
+                                        dir: uri,
+                                        files: files
+                                    }
+                                };
+                            }
+                            res.write(JSON.stringify(rescon) + "\n");
+                            res.end();
+                    });
+                        } else {
+                            // se la richiesta punta ad altro oggetto: es. symbolic link
+                            res.writeHead(403, {
+                                'Content-Type': 'text/plain'
+                            });
+                            res.write('Request Forbidden \n');
+                            res.end();
+                        }
+                    });
+                console.log(req.url);
+                console.log(process.cwd());
             }
-            // se la richiesta corrisponde a un file
-            if (stats.isFile()) {
-                var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
+            console.log("Server is available \n");
 
-                res.writeHead(200, {
-                    'Content-Type': mimeType
-                });
-                // crea uno stream dal file e lo va a scrivere all 'interno di res
-                var fileStream = fs.createReadStream(filename);
-                fileStream.pipe(res);
-            } else if (stats.isDirectory()) {
-                // se la richiesta corrisponde a un file directory
+            //variabili d'ambiente OpenShift
+            var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8383;
+            var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-                res.writeHead(200, {
-                    'Content-Type': 'text/plain'
-                });
-                res.write('La risorsa ' + uri + ' è una directory' + '\n');
-                //!!!!!!!!!!!! Ritornare oggetto JSON con contenuto della directory
-                res.end();
-            } else {
-                // se la richiesta punta ad altro oggetto: es. symbolic link
-                res.writeHead(403, {
-                    'Content-Type': 'text/plain'
-                });
-                res.write('Request Forbidden \n');
-                res.end();
-            }
-        });
-    console.log(req.url);
-    console.log(process.cwd());
-}
-console.log("Server is available \n");
-
-//variabili d'ambiente OpenShift
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8383;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-//creazione server
-var server = http.createServer(processa);
-server.listen(server_port, server_ip_address, function () {
-    console.log("Listening on " + server_ip_address + ", server_port " + server_port)
-});
+            //creazione server
+            var server = http.createServer(processa); server.listen(server_port, server_ip_address, function () {
+                console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+            });
